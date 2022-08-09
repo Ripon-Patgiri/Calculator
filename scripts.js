4e2f0c16a92c3a438f4816aa1ea79010d316aef6
Ripon-Patgiri <riponmongzeell@gmail.com>
2022-08-09 18:02:16 +0530

    Basic add,sub,mult,div function working.

---
+++ scripts.js
@@ -0,0 +1,24 @@
+
+let num1 = parseInt(prompt('Num 1: '));
+let num2 = parseInt(prompt('Num 2: '));
+
+function add(num1,num2) {
+    console.log(num1 + num2);
+}
+
+function subtract(num1,num2) {
+    console.log(num1 - num2);
+}
+
+function multiply(num1,num2) {
+    console.log(num1 * num2);
+}
+
+function divide(num1,num2) {
+    console.log(num1 / num2);
+}
+
+add(num1,num2);
+subtract(num1,num2);
+multiply(num1,num2);
+divide(num1,num2);
